1b9b53ac139372fcabeb2b9307367a20b2e35492
Luis Felipe <luisfelipearaujo503@gmail.com>
2025-01-20 23:28:52 -0300

    codigo testado e pronto pra ser apresentado

--- script.js
+++ script.js
@@ -1,60 +1,29 @@
-const menu = document.querySelector("#nav-section");
-const menuItems = document.querySelectorAll(".nav-link");
-let hamburger= document.querySelector(".hamburger");
-alert(hamburger);
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-
+// Function to toggle the navigation menu
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "flex";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "flex";
-    menuIcon.style.display = "none";
-  }
-};
-const navSection = document.querySelector('nav');
+    const navSection = document.querySelector('#nav-section');
+    navSection.classList.toggle('active');
+    
+}
 
-// Function to check screen width and add/remove the haMBURGUER  
-function showHamburger(){
-    if (window.innerWidth < 790) {
+// Function to adjust the menu display based on screen width
+function adjustMenu() {
+    const navSection = document.querySelector('#nav-section');
+    const hamburger = document.querySelector('.hamburger');
+    if (window.innerWidth > 800) {
+        hamburger.style.display = 'none';
+        navSection.classList.remove('active');
+    } else {
         
-        if (!hamburger) {
-            alert("NONE");
-            hamburger = document.querySelector(".hamburger");
-            hamburger.addEventListener("click", toggleMenu);
-            // Create the img element
-            
-      
-            // Apply CSS styles directly or via a class
-            imgElement.style.width = '50px'; // Example inline style
-            imgElement.style.height = 'auto';
-            imgElement.style.margin = '10px';
-      
-            // Append the img to the nav section
-            navSection.appendChild(imgElement);
-          }
-    }
-    else {
+        hamburger.style.display = 'flex';
         
-        // Remove the image if the width is greater than or equal to 790px
-        if (hamburger) {
-            
-            hamburger.remove();
-        }
     }
 }
-menuItems.forEach( 
 
-    function(menuItem) { 
-  
-      menuItem.addEventListener("click", toggleMenu);
-  
-    }
-  
-  )
+// Adjust menu on window resize
+window.addEventListener('resize', adjustMenu);
+// Initial adjustment
+adjustMenu();
 
-window.addEventListener("resize", showHamburger);
+
+
+//document.querySelector('.closeIcon').style.display = "none";
